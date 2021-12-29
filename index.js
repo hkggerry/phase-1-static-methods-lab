03247d08d080606477354602690d76266a161b43
gerry <hkggerry@gmail.com>
2021-12-29 11:14:35 -0800

    commit tree 76ebfe88e2d9a6b99f92547291acbe74bb0eec1f

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^\w\s\-\']/gi, '')
+  }
+
+static titleize(string) {
+  const excludeWords = ['of', 'a', 'an', 'but', 'the', 'for', 'at', 'by', 'from', 'and']
+  return string.replace(/\w\S*/g, function (txt) {
+    if (txt === string[0]) return txt.toUpperCase()
+    if (excludeWords.indexOf(txt) === -1) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+    return txt
+  }
+  )
+}
 }
